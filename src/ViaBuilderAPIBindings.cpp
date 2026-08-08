6a6944a0c7cfc8709cfa253bf7248843319d6edb
Thomas <154033411+Kelexer1@users.noreply.github.com>
2026-08-08 17:02:55 -0400

    Added formatting using clangd. Updated formatting across codebase.

--- src/ViaBuilderAPIBindings.cpp
+++ src/ViaBuilderAPIBindings.cpp
@@ -1,18 +1,17 @@
 #ifdef __EMSCRIPTEN__
-#include <emscripten/bind.h>
 #include "ViaBuilderAPI.h"
+#include <emscripten/bind.h>
 
 using namespace emscripten;
 
 EMSCRIPTEN_BINDINGS(builder_api_module) {
-    class_<ViaBuilderAPI>("ViaBuilderAPI")
-        .constructor<>()
-        .function("setPreferences", &ViaBuilderAPI::setPreferences)
-        .function("setSettings", &ViaBuilderAPI::setSettings)
-        .function("addCourse", &ViaBuilderAPI::addCourse)
-        .function("removeCourse", &ViaBuilderAPI::removeCourse)
-        .function("buildTimetable", &ViaBuilderAPI::buildTimetable)
-        .function("getAddedCourses", &ViaBuilderAPI::getAddedCourses)
-        ;
+  class_<ViaBuilderAPI>("ViaBuilderAPI")
+      .constructor<>()
+      .function("setPreferences", &ViaBuilderAPI::setPreferences)
+      .function("setSettings", &ViaBuilderAPI::setSettings)
+      .function("addCourse", &ViaBuilderAPI::addCourse)
+      .function("removeCourse", &ViaBuilderAPI::removeCourse)
+      .function("buildTimetable", &ViaBuilderAPI::buildTimetable)
+      .function("getAddedCourses", &ViaBuilderAPI::getAddedCourses);
 }
 #endif
